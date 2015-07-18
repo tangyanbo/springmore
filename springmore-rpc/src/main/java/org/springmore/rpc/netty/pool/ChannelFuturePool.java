13c86fe99eef5242caa918607abed68dfa2ec8ed
tangyanbo <tangyanbo1110@126.com>
2015-07-18 23:22:00 +0800

    commit

--- springmore-rpc/src/main/java/org/springmore/rpc/netty/pool/ChannelFuturePool.java
+++ springmore-rpc/src/main/java/org/springmore/rpc/netty/pool/ChannelFuturePool.java
@@ -6,14 +6,19 @@ import io.netty.channel.ChannelFuture;
 
 /**
  * ChannelFuturePool
+ * 
  * @author 唐延波
  * @date 2015年7月18日
  */
 public class ChannelFuturePool extends Pool<ChannelFuture> {
 
-	public ChannelFuturePool(final GenericObjectPoolConfig poolConfig,String host,int port){
-		
-		super(poolConfig,new ChannelFutureFactory(host,port));
+	public ChannelFuturePool(final GenericObjectPoolConfig poolConfig, String host, int port) {
+
+		super(poolConfig, new ChannelFutureFactory(host, port));
 	}
 
+	public ChannelFuturePool(String host, int port) {
+
+		super(new GenericObjectPoolConfig(), new ChannelFutureFactory(host, port));
+	}
 }
